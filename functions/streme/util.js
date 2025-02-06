50db4e54cfdc4ad2c003ff8e45aa0bd8cb8bd338
markcarey <mark@markcarey.com>
2025-02-05 23:33:30 -0500

    contract ABIs

--- functions/streme/util.js
+++ functions/streme/util.js
@@ -11,3 +11,11 @@ const {
 const ethers = require("ethers");
 const {PubSub} = require("@google-cloud/pubsub");
 const pubsub = new PubSub();
+
+const StremeJSON = require("./abis/Streme.json");
+const StakingFactoryJSON = require("./abis/StakingFactory.json");
+const StakedTokenJSON = require("./abis/StakedToken.json");
+const LPFactoryJSON = require("./abis/LPFactory.json");
+const LpLockerJSON = require("./abis/LpLockerv2.json");
+const SuperTokenFactoryJSON = require("./abis/SuperTokenFactory.json");
+
